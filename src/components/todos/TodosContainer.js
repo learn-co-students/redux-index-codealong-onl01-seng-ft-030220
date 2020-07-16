750de9c94f61599a13e3425681346e2de4e68d2e
pharia-le <pharia.le@gmail.com>
2020-07-15 18:52:56 -0700

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
- 
+import Todo from './Todo'
+
 class TodosContainer extends Component {
     
   renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
